292c0c17aed70d6507231816e2de495a9c156070
Mrkn-Sh <hansimss0201@gmail.com>
2022-03-25 16:57:48 +0900

    add footer

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="header">
+    <div class="header__logo">
+      <!-- <img src="" /> -->
+    </div>
+    <div class="header__alert">
+      
+    </div>
+  </div>
+</template>
---
+++ pages/setup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header />
+    <div>setup page!</div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+
+export default defineComponent({
+  components: {
+    Header,
+    Footer,
+  },
+  setup() {
+        
+  },
+})
+</script>
