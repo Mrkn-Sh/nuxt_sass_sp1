7a03f4796c2165cc2bb03f52adf2db6d3f2735ca
Mrkn-Sh <hansimss0201@gmail.com>
2022-03-25 23:38:28 +0900

    add setup.vue

--- pages/setup.vue
+++ pages/setup.vue
@@ -1,8 +1,35 @@
 <template>
   <div>
     <Header />
-    <div>setup page!</div>
-    <Footer />
+    <div class="setup">
+      <div class="setup__body">
+        <dl class="setup__list">
+          <dt>설정</dt>
+          <dd>
+            <button class="btn btn--medium btn--flat_orange">프로필</button>
+          </dd>
+          <dd>
+            <button class="btn btn--medium btn--flat_orange">메일</button>
+          </dd>
+          <dd>
+            <button class="btn btn--medium btn--flat_orange">비번</button>
+          </dd>
+          <dd>
+            <button class="btn btn--medium btn--flat_orange">상담</button>
+          </dd>
+          <dd>
+            <button class="btn btn--medium btn--flat_orange">이용내역</button>
+          </dd>                
+          <dd>
+            <button class="btn btn--medium btn--flat_orange">프라이버시</button>
+          </dd>
+          <dd>
+            <button class="btn btn--medium btn--flat_orange">로그아웃</button>
+          </dd>
+        </dl>
+      </div>
+    </div>
+  <Footer />
   </div>
 </template>
 
