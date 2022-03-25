c32274493e27f139771a62e5dc69f33b00173ba9
Mrkn-Sh <hansimss0201@gmail.com>
2022-03-25 14:50:03 +0900

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <Tutorial />
+  <div>
+    <div><input type="text" placeholder="입력하슈" /></div>
+    <div><input type="text" /></div>
+    <div><button class="btn btn--large btn--submit">눌러</button></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'IndexPage'
 }
 </script>
