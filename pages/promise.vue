2d7528d74f30a161c05ac963d9097337313f12bb
Mrkn-Sh <hansimss0201@gmail.com>
2022-03-28 16:26:48 +0900

    add promise

---
+++ pages/promise.vue
@@ -0,0 +1,234 @@
+<template>
+  <div>
+    <Header />
+    <ul class="promise">
+      <li>
+        <div class="promise__contents">
+          <div class="promise__contents__title">
+            초대하고 싶은 이벤트
+          </div>
+          <div class="promise__contents__image">
+            <img src="" width="100%"/>
+          </div>
+          <div class="promise__contents__subtitle">
+            <i class="icon icon--beginner" />
+            <span class="promise__contents__subtitle__text">컨텐츠1</span>
+          </div>
+          <div class="promise__contents__badge">
+            <span class="badge badge--green">서브컨텐츠1</span>
+          </div>
+          <div class="promise__contents__date">
+            4월1일 18:30~20:00
+          </div>
+          <div class="promise__contents__comment">
+            <div class="promise__contents__comment__avatars">
+              <ul class="promise__contents__comment__avatars__list">
+                <li>
+                  <div class="promise__contents__avatars">
+                    <div class="promise__contents__avatars__image">
+                      <img  src="" width="34" height="34" />
+                    </div>
+                     <div class="promise__contents__avatars__text">회원1</div>
+                  </div>
+                </li>
+                <li>
+                  <div class="promise__contents__avatars">
+                    <div class="promise__contents__avatars__image">
+                      <img  src="" width="34" height="34" />
+                    </div>
+                     <div class="promise__contents__avatars__text">회원2</div>
+                  </div></li>
+              </ul>
+            </div>
+            <div class="promise__contents__comment__text"></div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="promise__contents">
+          <div class="promise__contents__title">
+            초대하고 싶은 이벤트
+          </div>
+          <div class="promise__contents__image">
+            <img src="" width="100%"/>
+          </div>
+          <div class="promise__contents__subtitle">
+            <span class="promise__contents__subtitle__text">컨텐츠1</span>
+          </div>
+          <div class="promise__contents__badge">
+            <span class="badge badge--yellow">서브컨텐츠1</span>
+          </div>
+          <div class="promise__contents__date">
+            4월1일 18:30~20:00
+          </div>
+          <div class="promise__contents__comment">
+            <div class="promise__contents__comment__avatars">
+              <ul class="promise__contents__comment__avatars__list">
+                <li>
+                  <div class="promise__contents__avatars">
+                    <div class="promise__contents__avatars__image">
+                      <img  src="" width="34" height="34" />
+                    </div>
+                     <div class="promise__contents__avatars__text">회원1</div>
+                  </div>
+                </li>
+                <li>
+                  <div class="promise__contents__avatars">
+                    <div class="promise__contents__avatars__image">
+                      <img  src="" width="34" height="34" />
+                    </div>
+                     <div class="promise__contents__avatars__text">회원2</div>
+                  </div></li>
+              </ul>
+            </div>
+            <div class="promise__contents__comment__text"></div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="promise__contents">
+          <div class="promise__contents__title">
+            초대하고 싶은 이벤트
+          </div>
+          <div class="promise__contents__image">
+            <img src="" width="100%"/>
+          </div>
+          <div class="promise__contents__subtitle">
+            <i class="icon icon--beginner" />
+            <span class="promise__contents__subtitle__text">컨텐츠1</span>
+          </div>
+          <div class="promise__contents__badge">
+            <span class="badge badge--sky">서브컨텐츠1</span>
+          </div>
+          <div class="promise__contents__date">
+            4월1일 18:30~20:00
+          </div>
+          <div class="promise__contents__comment">
+            <div class="promise__contents__comment__avatars">
+              <ul class="promise__contents__comment__avatars__list">
+                <li>
+                  <div class="promise__contents__avatars">
+                    <div class="promise__contents__avatars__image">
+                      <img  src="" width="34" height="34" />
+                    </div>
+                     <div class="promise__contents__avatars__text">회원1</div>
+                  </div>
+                </li>
+                <li>
+                  <div class="promise__contents__avatars">
+                    <div class="promise__contents__avatars__image">
+                      <img  src="" width="34" height="34" />
+                    </div>
+                     <div class="promise__contents__avatars__text">회원2</div>
+                  </div></li>
+              </ul>
+            </div>
+            <div class="promise__contents__comment__text"></div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="promise__contents">
+          <div class="promise__contents__title">
+            초대하고 싶은 이벤트
+          </div>
+          <div class="promise__contents__image">
+            <img src="" width="100%"/>
+          </div>
+          <div class="promise__contents__subtitle">
+            <i class="icon icon--beginner" />
+            <span class="promise__contents__subtitle__text">컨텐츠1</span>
+          </div>
+          <div class="promise__contents__badge">
+            <span class="badge badge--purple">서브컨텐츠1</span>
+          </div>
+          <div class="promise__contents__date">
+            4월1일 18:30~20:00
+          </div>
+          <div class="promise__contents__comment">
+            <div class="promise__contents__comment__avatars">
+              <ul class="promise__contents__comment__avatars__list">
+                <li>
+                  <div class="promise__contents__avatars">
+                    <div class="promise__contents__avatars__image">
+                      <img  src="" width="34" height="34" />
+                    </div>
+                     <div class="promise__contents__avatars__text">회원1</div>
+                  </div>
+                </li>
+                <li>
+                  <div class="promise__contents__avatars">
+                    <div class="promise__contents__avatars__image">
+                      <img  src="" width="34" height="34" />
+                    </div>
+                     <div class="promise__contents__avatars__text">회원2</div>
+                  </div></li>
+              </ul>
+            </div>
+            <div class="promise__contents__comment__text"></div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="promise__contents">
+          <div class="promise__contents__title">
+            초대하고 싶은 이벤트
+          </div>
+          <div class="promise__contents__image">
+            <img src="" width="100%"/>
+          </div>
+          <div class="promise__contents__subtitle">
+            <i class="icon icon--beginner" />
+            <span class="promise__contents__subtitle__text">컨텐츠1</span>
+          </div>
+          <div class="promise__contents__badge">
+            <span class="badge badge--blue">서브컨텐츠1</span>
+          </div>
+          <div class="promise__contents__date">
+            4월1일 18:30~20:00
+          </div>
+          <div class="promise__contents__comment">
+            <div class="promise__contents__comment__avatars">
+              <ul class="promise__contents__comment__avatars__list">
+                <li>
+                  <div class="promise__contents__avatars">
+                    <div class="promise__contents__avatars__image">
+                      <img  src="" width="34" height="34" />
+                    </div>
+                     <div class="promise__contents__avatars__text">회원1</div>
+                  </div>
+                </li>
+                <li>
+                  <div class="promise__contents__avatars">
+                    <div class="promise__contents__avatars__image">
+                      <img  src="" width="34" height="34" />
+                    </div>
+                     <div class="promise__contents__avatars__text">회원2</div>
+                  </div></li>
+              </ul>
+            </div>
+            <div class="promise__contents__comment__text"></div>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  components: {
+    Header,
+    Footer,
+  },
+    setup() {
+        
+    },
+})
+</script>
